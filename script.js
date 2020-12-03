0b0c77a5da3cbd1ac084c55d6785094f94e623bc
Pedro Penna <ppenna@thoughtworks.com>
2020-12-03 19:57:29 -0300

    Change how the language section is filled

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 (function() {
+  'use strict';
+
   let data = {
     name: 'Pedro Luiz Moreira Penna',
     email: 'pedropenna@gmail.com',
     linkedIn: 'https://www.linkedin.com/in/pedropenna',
-    phone: '+55 31 9-9279-6486'
+    phone: '+55 31 9-9279-6486',
+
+    languages: [
+      {
+        name: 'English',
+        level: 'Full Professional Proficiency (Fluent)'
+      },
+      {
+        name: 'Spanish',
+        level: 'Limited Professional Proficiency'
+      },
+      {
+        name: 'Portuguese',
+        level: 'Native'
+      }
+    ]
   };
 
   let nameHtml = data.name
       .toUpperCase()
       .split(' ')
-      .reduce((rest, word) => {
-        return rest + ' <span class="capNome">' + word.substring(0, 1) + '</span>' + word.substring(1);
-      });
-  console.log('nameHtml', nameHtml);
+      .map((word) => {
+        return '<span class="capNome">' + word.substring(0, 1) + '</span>' + word.substring(1);
+      })
+      .join(' ');
 
   document.getElementById("name").innerHTML = nameHtml;
   document.getElementById("email").innerText = data.email;
@@ -21,4 +38,14 @@
   document.getElementById("phone").innerHTML = data.phone;
 
 
+  let languagesHtml = data.languages
+      .map((language) => {
+        return '<li>' + language.name + separator() + language.level + '</li>';
+      })
+      .join('');
+  document.getElementById("languages").innerHTML = languagesHtml;
+
+  function separator() {
+    return '<span class="separador">&mdash;</span>';
+  }
 })();
