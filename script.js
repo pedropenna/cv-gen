72798f195653fb37f8d93181150f1e2899e52df2
Pedro Penna <ppenna@thoughtworks.com>
2020-12-03 20:28:33 -0300

    Change how the education section is filled

--- script.js
+++ script.js
@@ -7,6 +7,12 @@
     linkedIn: 'https://www.linkedin.com/in/pedropenna',
     phone: '+55 31 9-9279-6486',
 
+    education: [
+      {
+        degree: 'Bachelor\'s degree in Computer Science',
+        location: 'Pontif\u00EDcia Universidade Cat\u00F3lica de Minas Gerais'
+      }
+    ],
     languages: [
       {
         name: 'English',
@@ -37,15 +43,30 @@
   document.getElementById("linkedIn").href = data.linkedIn;
   document.getElementById("phone").innerHTML = data.phone;
 
+  let education = data.education
+      .map(({degree, location}) => nameDescriptionItemList(degree, location))
+      .map((item) => {
+        document.getElementById("education").appendChild(item);
+      });
 
-  let languagesHtml = data.languages
-      .map((language) => {
-        return '<li>' + language.name + separator() + language.level + '</li>';
-      })
-      .join('');
-  document.getElementById("languages").innerHTML = languagesHtml;
+  let languages = data.languages
+      .map(({name, level}) => nameDescriptionItemList(name, level))
+      .map((item) => {
+           document.getElementById("languages").appendChild(item);
+      });
 
   function separator() {
-    return '<span class="separador">&mdash;</span>';
+    let span = document.createElement('span');
+    span.className = 'separador';
+    span.innerHTML = '&mdash;';
+    return span;
+  }
+
+  function nameDescriptionItemList(name, description) {
+    let list = document.createElement('li');
+    list.appendChild(document.createTextNode(name));
+    list.appendChild(separator());
+    list.appendChild(document.createTextNode(description));
+    return list;
   }
 })();
