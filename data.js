cb65858ab2eb2f0b54947e70ab49725f4b70d1b2
Pedro Penna <ppenna@thoughtworks.com>
2020-12-04 11:51:26 -0300

    Separate data JSON from the logic script

---
+++ data.js
@@ -0,0 +1,134 @@
+
+'use strict';
+
+let data = {
+  name: 'Pedro Luiz Moreira Penna',
+  email: 'pedropenna@gmail.com',
+  linkedIn: 'https://www.linkedin.com/in/pedropenna',
+  phone: '+55 31 9-9279-6486',
+
+  experience: [
+    {
+      company: 'ThoughtWorks',
+      startMonth: 9,
+      startYear: 2017,
+      role: 'Senior Consultant Developer',
+      activity: 'Worked on the development of multiple microservices for a large airline company',
+      tech: 'Java, JavaScript, Spring Boot, Splunk, Jenkins, OWASP Zed Attack Proxy, OWASP Dependency Check, PiTest, Sonar, Intellij, Agile Software Development, Git, JUnit and Hamcrest',
+      methodology: 'Agile, Kanban, Continuous Integration and Test-Driven Development',
+    },
+    {
+      company: 'Sydle',
+      startMonth: 6,
+      startYear: 2016,
+      endMonth: 9,
+      endYear: 2017,
+      role: 'Software Engineer',
+      activity: 'Worked on the development of a new product, that was critical to the success of the company\'s new strategy',
+      tech: 'Angular 4, Java, MongoDB, JavaScript, TypeScript, HTML/CSS, SCSS, jQuery, Eclipse, Tomcat, Ivy, Git, JUnit and REST Assured',
+      methodology: 'Scrum and Test-Driven Development',
+    },
+    {
+      company: 'ETEG',
+      startMonth: 10,
+      startYear: 2010,
+      endMonth: 12,
+      endYear: 2015,
+      role: 'Software Developer/Architect',
+      activity: 'Worked on-site on the evolution of the hospital management system\'s architecture for FHEMIG, the state hospital foundation that is responsible for the largest public hospital network in Latin America. Also worked as the team\'s tech lead and on the configuration, server provisioning and the system monitoring',
+      tech: 'Java, Spring, Hibernate, Tapestry 5, JSP/Servlet, Groovy, Grails, Velocity, Sitemesh, Acegi/Spring Security, AOP, HTML/CSS, JavaScript, IntelliJ IDEA, Eclipse, Apache Tomcat, Apache HTTP Server, Mod_JK, JBoss, Jetty, Ant, Maven, Junit, TestNG, Coverage, Sonar, Hudson, SVN, Git, Xen Hypervisor and MySQL',
+      methodology: 'Scrum',
+    },
+    {
+      company: 'iFactory',
+      startMonth: 6,
+      startYear: 2010,
+      endMonth: 10,
+      endYear: 2010,
+      role: 'Software Developer',
+      activity: 'Worked remotely (from home) as part of the development team responsible for the Oxford English Dictionary website and search feature',
+      tech: 'Java, Tapestry 5, XPath, XQuery, Jetty, Eclipse, jUnit and Selenium',
+    },
+    {
+      company: 'Samba Tech',
+      startMonth: 1,
+      startYear: 2010,
+      endMonth: 6,
+      endYear: 2010,
+      role: 'Software Developer',
+      activity: 'Worked on the company\'s main media management product',
+      tech: 'Java, Spring, AOP, Hibernate, Eclipse, SpringSource Tool Suite, GraniteDS, BlazeDS, Tomcat, Ant, Maven, Junit and MySQL',
+      methodology: 'Scrum',
+    },
+    {
+      company: 'ETEG',
+      startMonth: 11,
+      startYear: 2004,
+      endMonth: 1,
+      endYear: 2010,
+      role: 'Software Developer/Architect',
+      activities: [
+        {
+          startYear: 2004,
+          endYear: 2007,
+          description: 'Created and evolved a financial system\'s architecture for a financial company and led a team of developers',
+        },
+        {
+          startYear: 2007,
+          endYear: 2010,
+          description: 'Worked on-site on the evolution of the hospital management system\'s architecture for FHEMIG, the state hospital foundation that is responsible for the largest public hospital network in Latin America. Also worked as the team\'s tech lead and on the configuration, server provisioning and the system monitoring',
+        }
+      ],
+      tech: 'Java, Spring, Hibernate, Tapestry 5, Struts, JSP/Servlet, Groovy, Grails, Velocity, Sitemesh, Acegi/Spring Security, AOP, HTML/CSS, JavaScript, Eclipse, IntelliJ IDEA, Tomcat, JBoss, Jetty, Ant, Maven, JUnit, Coverage, SVN, MySQL and SQL Server',
+    },
+    {
+      company: 'Pentagrama Consultoria e Sistemas',
+      startMonth: 2,
+      startYear: 2004,
+      endMonth: 10,
+      endYear: 2004,
+      role: 'Software Developer',
+      activity: 'Worked alone (and on-site) in the development of several systems for a clinical laboratory',
+      tech: 'Java, JSP/Servlet, Struts, HTML, JavaScript, Applet, XML/XSLT, Eclipse, Tomcat, MySQL and Cach\u00E9',
+    },
+    {
+      company: 'Squadra Tecnologia',
+      startMonth: 2,
+      startYear: 2003,
+      endMonth: 2,
+      endYear: 2004,
+      role: 'Front-End/Web Developer',
+      activity: 'Development of multiple web applications',
+      tech: 'Java, JSP/Servlet, Struts, HTML, JavaScript, Eclipse, ASP, Delphi, Oracle, JBuilder, JBoss and Websphere',
+    }
+  ],
+
+  certifications: [
+    'Core Spring 3 Certification',
+    'Sun Certified Enterprise Architect',
+    'Sun Certified Business Component Developer',
+    'Sun Certified Web Component Developer',
+    'Sun Certified Java Programmer'
+  ],
+  education: [
+    {
+      degree: 'Bachelor\'s degree in Computer Science',
+      location: 'Pontif\u00EDcia Universidade Cat\u00F3lica de Minas Gerais'
+    }
+  ],
+  languages: [
+    {
+      name: 'English',
+      level: 'Full Professional Proficiency (Fluent)'
+    },
+    {
+      name: 'Spanish',
+      level: 'Limited Professional Proficiency'
+    },
+    {
+      name: 'Portuguese',
+      level: 'Native'
+    }
+  ]
+};
+
--- script.js
+++ script.js
@@ -1,98 +1,9 @@
 (function() {
   'use strict';
 
-  let data = {
-    name: 'Pedro Luiz Moreira Penna',
-    email: 'pedropenna@gmail.com',
-    linkedIn: 'https://www.linkedin.com/in/pedropenna',
-    phone: '+55 31 9-9279-6486',
+  let resume = data || {};
 
-    experience: [
-      {
-        company: 'ThoughtWorks',
-        startMonth: 9,
-        startYear: 2017,
-        role: 'Senior Consultant Developer',
-        activity: 'Worked on the development of multiple microservices for a large airline company',
-        tech: 'Java, JavaScript, Spring Boot, Splunk, Jenkins, OWASP Zed Attack Proxy, OWASP Dependency Check, PiTest, Sonar, Intellij, Agile Software Development, Git, JUnit and Hamcrest',
-        methodology: 'Agile, Kanban, Continuous Integration and Test-Driven Development',
-      },
-      {
-        company: 'Sydle',
-        startMonth: 6,
-        startYear: 2016,
-        endMonth: 9,
-        endYear: 2017,
-        role: 'Software Engineer',
-        activity: 'Worked on the development of a new product, that was critical to the success of the company\'s new strategy',
-        tech: 'Angular 4, Java, MongoDB, JavaScript, TypeScript, HTML/CSS, SCSS, jQuery, Eclipse, Tomcat, Ivy, Git, JUnit and REST Assured',
-        methodology: 'Scrum and Test-Driven Development',
-      },
-      {
-        company: 'ETEG',
-        startMonth: 10,
-        startYear: 2010,
-        endMonth: 12,
-        endYear: 2015,
-        role: 'Software Developer/Architect',
-        activity: 'Worked on-site on the evolution of the hospital management system\'s architecture for FHEMIG, the state hospital foundation that is responsible for the largest public hospital network in Latin America. Also worked as the team\'s tech lead and on the configuration, server provisioning and the system monitoring',
-        tech: 'Java, Spring, Hibernate, Tapestry 5, JSP/Servlet, Groovy, Grails, Velocity, Sitemesh, Acegi/Spring Security, AOP, HTML/CSS, JavaScript, IntelliJ IDEA, Eclipse, Apache Tomcat, Apache HTTP Server, Mod_JK, JBoss, Jetty, Ant, Maven, Junit, TestNG, Coverage, Sonar, Hudson, SVN, Git, Xen Hypervisor and MySQL',
-        methodology: 'Scrum',
-      },
-      {
-        company: 'iFactory',
-        startMonth: 6,
-        startYear: 2010,
-        endMonth: 10,
-        endYear: 2010,
-        role: 'Software Developer',
-        activity: 'Worked remotely (from home) as part of the development team responsible for the Oxford English Dictionary website and search feature',
-        tech: 'Java, Tapestry 5, XPath, XQuery, Jetty, Eclipse, jUnit and Selenium',
-      },
-      {
-        company: 'Samba Tech',
-        startMonth: 1,
-        startYear: 2010,
-        endMonth: 6,
-        endYear: 2010,
-        role: 'Software Developer',
-        activity: 'Worked on the company\'s main media management product',
-        tech: 'Java, Spring, AOP, Hibernate, Eclipse, SpringSource Tool Suite, GraniteDS, BlazeDS, Tomcat, Ant, Maven, Junit and MySQL',
-        methodology: 'Scrum',
-      },
-      
-    ],
-
-    certifications: [
-      'Core Spring 3 Certification',
-      'Sun Certified Enterprise Architect',
-      'Sun Certified Business Component Developer',
-      'Sun Certified Web Component Developer',
-      'Sun Certified Java Programmer'
-    ],
-    education: [
-      {
-        degree: 'Bachelor\'s degree in Computer Science',
-        location: 'Pontif\u00EDcia Universidade Cat\u00F3lica de Minas Gerais'
-      }
-    ],
-    languages: [
-      {
-        name: 'English',
-        level: 'Full Professional Proficiency (Fluent)'
-      },
-      {
-        name: 'Spanish',
-        level: 'Limited Professional Proficiency'
-      },
-      {
-        name: 'Portuguese',
-        level: 'Native'
-      }
-    ]
-  };
-
-  let nameHtml = data.name
+  let nameHtml = resume.name
       .toUpperCase()
       .split(' ')
       .map((word) => {
@@ -101,12 +12,12 @@
       .join(' ');
 
   document.getElementById("name").innerHTML = nameHtml;
-  document.getElementById("email").innerText = data.email;
-  document.getElementById("linkedIn").innerHTML = data.linkedIn;
-  document.getElementById("linkedIn").href = data.linkedIn;
-  document.getElementById("phone").innerHTML = data.phone;
+  document.getElementById("email").innerText = resume.email;
+  document.getElementById("linkedIn").innerHTML = resume.linkedIn;
+  document.getElementById("linkedIn").href = resume.linkedIn;
+  document.getElementById("phone").innerHTML = resume.phone;
 
-  let experience = data.experience
+  let experience = resume.experience
       .forEach((experience) => {
         var row = document.createElement("tr");
         var cell = document.createElement("td");
@@ -121,19 +32,19 @@
         document.getElementById('experience').appendChild(row);
       });
 
-  let certifications = data.certifications
+  let certifications = resume.certifications
       .map((certification) => createSimpleItemList(certification))
       .forEach((item) => {
         document.getElementById('certifications').appendChild(item);
       });
 
-  let education = data.education
+  let education = resume.education
       .map(({degree, location}) => createNameDescriptionItemList(degree, location))
       .forEach((item) => {
         document.getElementById('education').appendChild(item);
       });
 
-  let languages = data.languages
+  let languages = resume.languages
       .map(({name, level}) => createNameDescriptionItemList(name, level))
       .forEach((item) => {
            document.getElementById('languages').appendChild(item);
@@ -160,6 +71,22 @@
     return list;
   }
 
+  function createItemList(text, child) {
+    let list = document.createElement('li');
+    list.appendChild(document.createTextNode(text));
+    list.appendChild(child);
+    return list;
+  }
+
+  function createItemListWithMultipleChildren(children) {
+    let list = document.createElement('li');
+    children.forEach((child) => {
+      list.appendChild(child);
+    });
+
+    return list;
+  }
+
   function createSpan(text, className) {
     let span = document.createElement('span');
     span.className = className;
@@ -173,12 +100,17 @@
     let fullDescription;
     if (experience.endYear) {
       let end = formatMonth(experience.endMonth) + ' ' + experience.endYear;
-      fullDescription = start + ' to ' + end;
+
+      if (experience.startYear) {
+        fullDescription = start + ' to ' + end;
+      } else {
+        fullDescription = 'Until ' + end;
+      }
     } else {
       fullDescription = 'Since ' + start;
     }
 
-    return createSpan(fullDescription, 'data');
+    return createSpan(fullDescription, 'resume');
   }
 
   function createList(listItems) {
@@ -193,8 +125,20 @@
     let experienceAttributes = [];
     let role = createSimpleItemList('Role: ' + experience.role + '.');
     experienceAttributes.push(role);
-    let activity = createSimpleItemList('Activity: ' + experience.activity + '.');
-    experienceAttributes.push(activity);
+    if (experience.activity) {
+      let activity = createSimpleItemList('Activity: ' + experience.activity + '.');
+      experienceAttributes.push(activity);
+    } else {
+      let nestedActivities = document.createElement("ul");
+      experience.activities
+        .forEach((activity) => {
+          let activityPeriod = createPeriod(activity);
+          let activityDescription = document.createTextNode(activity.description);
+          nestedActivities.appendChild(createItemListWithMultipleChildren([ activityPeriod, createSeparator(), activityDescription ]));
+        });
+      let activity = createItemList('Activity: ', nestedActivities);
+      experienceAttributes.push(activity);
+    }
     let tech = createSimpleItemList('Technology/Tools: ' + experience.tech + '.');
     experienceAttributes.push(tech);
     if (experience.methodology) {
@@ -205,6 +149,10 @@
   }
 
   function formatMonth(month) {
+    if (!month) {
+      return '';
+    }
+
     let longMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',];
     let shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
